bcb99aeb607dc4da820a31c7a84c2153ebeaaeeb
Meherazhossain91 <itsonlyopu@gmail.com>
2021-04-29 23:04:04 +0600

    first commit

--- index.php
+++ index.php
@@ -1,3 +1,39 @@
+<?php 
+
+$host="localhost";
+$user="root";
+$password="";
+$db="demo";
+
+mysql_connect($host,$user,$password);
+mysql_select_db($db);
+
+if(isset($_POST['username'])){
+    
+    $uname=$_POST['username'];
+    $password=$_POST['password'];
+    
+    $sql="select * from loginform where user='".$uname."'AND Pass='".$password."' limit 1";
+    
+    $result=mysql_query($sql);
+    
+    if(mysql_num_rows($result)==1){
+        echo " You Have Successfully Logged in";
+        exit();
+    }
+    else{
+        echo " You Have Entered Incorrect Password";
+        exit();
+    }
+        
+}
+?>
+
+
+
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -11,7 +47,7 @@
     <div class="loginbox">
         <img src="img/avater.jpg" class="avater">
         <h1>Login Here</h1>
-        <form>
+        <form method="POST" action="#">
 
             <p>User Name</p>
             <input type="text" name="" placeholder="Enter username">
